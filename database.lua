f625b0a3855a3a9d63521a9b777dc9f3fec4ce65
Brues <5278969+brues-code@users.noreply.github.com>
2026-08-19 09:03:41 -0500

    Reduce pfServerScan window to 100
    Change pfServerScan.window from 250 to 100 to limit the number of simultaneous server scan requests.
    
    (cherry picked from commit 74b69386c365fd2af27a8ec0f48f916c5161f711)

--- database.lua
+++ database.lua
@@ -2106,7 +2106,7 @@ pfServerScan:Hide()
 -- refused for lack of room.
 pfServerScan.scanID = 1
 pfServerScan.max = 100000
-pfServerScan.window = 250
+pfServerScan.window = 100
 pfServerScan.inflight = 0
 pfServerScan.scanPending = {}
 
